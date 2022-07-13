65696b5bfba6c7a5b99ad6837cc00535cd877e0b
Faheemuddin Naseem <77022255+Naseem010@users.noreply.github.com>
2022-07-13 21:25:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+var numbersofbutton = document.querySelectorAll(".drum").length;
+for (var i = 0; i < numbersofbutton; i++) {
+  // document.querySelectorAll("button")[i].style.color="white";
+  document.querySelectorAll("button")[i].addEventListener("click", function() {
+    var buttoninnerhtml = this.innerHTML;
+    makeSound(buttoninnerhtml);
+    buttonanimation(buttoninnerhtml);
+    }
+  );
+}
+
+document.addEventListener("keydown",function(event){
+      makeSound(event.key);
+      buttonanimation(event.key);
+});
+
+function makeSound(key){
+  switch (key){
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "l":
+      var kickbass = new Audio("sounds/kick-bass.mp3");
+      kickbass.play();
+      break;
+    default: console.log(key);
+}
+}
+
+// var audio=new Audio("sounds/tom-1.mp3");
+// audio.play();
+function buttonanimation(currkey){
+  var activebutton=document.querySelector("."+currkey);
+  activebutton.classList.add("pressed");
+  setTimeout(function() {
+    activebutton.classList.remove('pressed');
+}, 100);
+
+}
